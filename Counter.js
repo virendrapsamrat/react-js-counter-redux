be17fe599d1ba43d7fc9259390e89d67bfbdae46
virendrapsamrat <virendra.p.samrat@gmail.com>
2019-10-18 19:46:37 +0530

    adding

---
+++ Counter.js
@@ -0,0 +1,46 @@
+/*eslint-disable no-unused-vars */
+import React, { Component, PropTypes } from 'react'
+
+import NameForm from './Name'
+
+const Counter = ({ value, onIncrement, onDecrement, onIncrementFive, onIncrementIfOdd, clear, onIncrementAsync }) =>
+  <div>
+  <button onClick={onIncrementAsync}>
+      Increment after 1 second
+    </button>
+    {' '}
+    <button onClick={onIncrementFive}>
+      Increment 5 value
+    </button>
+    {' '}
+    <button onClick={onIncrement}>
+      Increment
+    </button>
+    {' '}
+    <button onClick={onDecrement}>
+      Decrement
+    </button>
+    {' '}
+    <button onClick={onIncrementIfOdd}>
+      Increment 1 If Odd
+    </button>
+    {' '}
+    <button onClick={clear}>
+      Clear
+    </button>
+    <hr />
+    <div>
+      Clicked: {value} times
+    </div>
+
+    <NameForm/> 
+  </div>
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
+  
